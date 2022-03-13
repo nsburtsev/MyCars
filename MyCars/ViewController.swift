05be2739a0bbe3dc5cce236038f30422c3b8e54b
Nyurgun <burtsev.ns@gmail.com>
2022-03-13 23:50:11 +0900

    Initial Commit

---
+++ MyCars/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyCars
+//
+//  Created by Нюргун on 13.03.2022.
+//  Copyright © 2022 Нюргун. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
